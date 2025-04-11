b14dcec5fc5845c58490b3d8e261f79c1b14a7b9
Alexis Soto Dimas <alexissotodimas09@gmail.com>
2025-04-11 12:10:59 -0600

    colors

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -9,10 +9,16 @@ class PasswordsController < ApplicationController
     if user = User.find_by(email_address: params[:email_address])
       user.generate_password_reset_token!
       PasswordsMailer.reset(user).deliver_later
+  
+      if Rails.env.development?
+        token = user.signed_id(purpose: "password_reset", expires_in: 15.minutes)
+        flash[:reset_link] = edit_password_url(token: token)
+      end
     end
-
-    redirect_to new_session_path, notice: "Password reset instructions sent (if user with that email address exists)."
+  
+    redirect_to root_path, notice: "Password reset instructions sent (if user with that email address exists)."
   end
+  
 
   def edit
   end
